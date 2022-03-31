a5c9a24e7cdb488cf6cacb7840de73a9d4ec3887
unknown <taha.iu.bd@gmail.com>
2022-03-31 14:39:20 +0600

    single product local storage add

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,6 +3,7 @@ import Product from '../Product/Product'
 import './Shop.css'
 
 import Cart from '../Cart/Cart'
+import { addToDb, getStorCart } from '../../utilities/fakedb'
 
 const Shop = () => {
     const [products, setProducts] = useState([])
@@ -12,8 +13,23 @@ const Shop = () => {
             .then((res) => res.json())
             .then((data) => setProducts(data))
     }, [])
+
+    useEffect(() => {
+        const storedCart = getStorCart()
+        const savedCart = []
+        for (const id in storedCart) {
+            const addedProduct = products.find((product) => product.id === id)
+            if (addedProduct) {
+                const quantity = storedCart[id]
+                addedProduct.quantity = quantity
+                savedCart.push(addedProduct)
+            }
+        }
+        setCart(savedCart)
+    }, [products])
     const handelCart = (product) => {
         setCart([...cart, product])
+        addToDb(product.id)
     }
     return (
         <div className="main-container">
