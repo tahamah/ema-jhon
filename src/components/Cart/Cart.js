a1b28b480b66b8b3020d94fa43138e88e420eb39
unknown <taha.iu.bd@gmail.com>
2022-03-31 13:59:21 +0600

    without grand total

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,15 +4,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight, faTrashCan } from '@fortawesome/free-solid-svg-icons'
 
 const Cart = ({ cart }) => {
+    let total = 0
+    let shipping = 0
+
+    for (const product of cart) {
+        total = total + product.price
+        shipping = shipping + product.shipping
+    }
+    const tax = (total * 0.1).toFixed(2)
     return (
         <div className="cart-summary ">
             <div className="cart">
                 <h5 className="summary-title ">Order Summary</h5>
                 <div className="summary-details">
                     <p>Selected Items: {cart.length}</p>
-                    <p>Total Price: $1140</p>
-                    <p>Total Shipping Charge: $5</p>
-                    <p>Tax: $114</p>
+                    <p>Total Price: $ {total}</p>
+                    <p>Total Shipping Charge: $ {shipping}</p>
+                    <p>Tax: $ {tax}</p>
                     <h5 className="grand-total">Grand Total: $1559</h5>
                 </div>
                 <div className="summary-btn">
