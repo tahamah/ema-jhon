db04a14d22c6324200b63cd0a81e800fb504ab56
unknown <taha.iu.bd@gmail.com>
2022-03-31 11:12:38 +0600

    order summary styky

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,27 +5,32 @@ import { faArrowRight, faTrashCan } from '@fortawesome/free-solid-svg-icons'
 
 const Cart = ({ cart }) => {
     return (
-        <div className="cart-summary">
-            <h5 className="summary-title">Order Summary</h5>
-            <div className="summary-details">
-                <p>Selected Items: {cart.length}</p>
-                <p>Total Price: $1140</p>
-                <p>Total Shipping Charge: $5</p>
-                <p>Tax: $114</p>
-                <h5 className="grand-total">Grand Total: $1559</h5>
-            </div>
-            <div className="summary-btn">
-                <button className="clear-btn">
-                    Clear Cart
-                    <FontAwesomeIcon className="cart-icon" icon={faTrashCan} />
-                </button>
-                <button className="review-btn">
-                    Review Order
-                    <FontAwesomeIcon
-                        className="cart-icon"
-                        icon={faArrowRight}
-                    />
-                </button>
+        <div className="cart-summary ">
+            <div className="cart">
+                <h5 className="summary-title ">Order Summary</h5>
+                <div className="summary-details">
+                    <p>Selected Items: {cart.length}</p>
+                    <p>Total Price: $1140</p>
+                    <p>Total Shipping Charge: $5</p>
+                    <p>Tax: $114</p>
+                    <h5 className="grand-total">Grand Total: $1559</h5>
+                </div>
+                <div className="summary-btn">
+                    <button className="clear-btn">
+                        Clear Cart
+                        <FontAwesomeIcon
+                            className="cart-icon"
+                            icon={faTrashCan}
+                        />
+                    </button>
+                    <button className="review-btn">
+                        Review Order
+                        <FontAwesomeIcon
+                            className="cart-icon"
+                            icon={faArrowRight}
+                        />
+                    </button>
+                </div>
             </div>
         </div>
     )
